f52d6b1c750b0e4a50131fa81dc6805cd277f5fc
Vova <vovalak@mail.ru>
2019-12-22 22:30:26 +0200

    game panel

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ import View from './src/view.js';
 const root = document.querySelector('#root');
 
 const game = new Game();
-const view = new View(root, 320, 640, 20, 10);
+const view = new View(root, 480, 640, 20, 10);
 
 window.game = game;
 window.view = view;
-
-console.log(game.getState());
 
 document.addEventListener('keydown', event => {
     switch(event.keyCode) {
@@ -31,7 +29,3 @@ document.addEventListener('keydown', event => {
             break;
     }
 });
-
-view.renderPlayfiled(game.getState());
-
-console.log(game);
