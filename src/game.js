8962f7010c89385d416c16271b8dcbd554addfc3
Vova <vovalak@mail.ru>
2019-12-22 20:28:07 +0200

    clear lines

--- src/game.js
+++ src/game.js
@@ -169,6 +169,7 @@ export default class Game {
         if (this.hasCollision()) {
             this.activePiece.y -=1;
             this.lockPiece();
+            this.clearLines();
             this.updatePieces();
         }
     }
@@ -200,6 +201,35 @@ export default class Game {
                     this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
                 }
             }
+        }
+    }
+
+    clearLines() {
+        const rows = 20;
+        const columns = 10;
+        let lines = [];
+
+        for (let y = rows - 1; y >= 0; y--) {
+            let numberOfBlocks = 0;
+
+            for (let x = 0; x < columns; x++) {
+                if (this.playfield[y][x]) {
+                    numberOfBlocks += 1;
+                }
+            }
+
+            if (numberOfBlocks === 0) {
+                break;
+            } else if (numberOfBlocks < columns) {
+                continue;
+            } else if (numberOfBlocks === columns) {
+                lines.unshift(y);
+            }
+        }
+
+        for (let index of lines) {
+            this.playfield.splice(index, 1);
+            this.playfield.unshift(new Array(columns).fill(0));
         }
     }
 
