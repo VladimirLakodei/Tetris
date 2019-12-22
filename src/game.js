492db9c1708a82b9e4a6de31b9ed3b210a92122e
Vova <vovalak@mail.ru>
2019-12-22 20:42:31 +0200

    game score

--- src/game.js
+++ src/game.js
@@ -1,10 +1,20 @@
 export default class Game {
+    static points = {
+        '1': 40,
+        '2': 100,
+        '3': 300,
+        '4': 1200
+    };
+
     score = 0;
-    lines = 0;
-    level = 0;
+    lines = 19;
     playfield = this.createPlayfield();
     activePiece = this.createPiece();
     nextPiece = this.createPiece();
+
+    get level() {
+        return Math.floor(this.lines * 0.1);
+    }
 
     getState() {
         const playfield = this.createPlayfield();
@@ -109,8 +119,6 @@ export default class Game {
         piece.x = Math.floor((10 - piece.blocks[0].length) / 2);
         piece.y = -1;
 
-        console.log('Piece type:', type);
-
         return piece;
     }
 
@@ -169,7 +177,8 @@ export default class Game {
         if (this.hasCollision()) {
             this.activePiece.y -=1;
             this.lockPiece();
-            this.clearLines();
+            const clearedLines = this.clearLines();
+            this.updateScore(clearedLines);
             this.updatePieces();
         }
     }
@@ -230,6 +239,15 @@ export default class Game {
         for (let index of lines) {
             this.playfield.splice(index, 1);
             this.playfield.unshift(new Array(columns).fill(0));
+        }
+
+        return lines.length;
+    }
+
+    updateScore(clearedLines) {
+        if (clearedLines > 0) {
+            this.score += Game.points[clearedLines] * (this.level + 1);
+            this.lines += clearedLines;
         }
     }
 
