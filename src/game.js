904ff225c4c5cc3ca4064ee93d330eda58bb9b2c
Vova <vovalak@mail.ru>
2019-12-25 18:21:41 +0200

    class Controller

--- src/game.js
+++ src/game.js
@@ -6,11 +6,9 @@ export default class Game {
         '4': 1200
     };
 
-    score = 0;
-    lines = 19;
-    playfield = this.createPlayfield();
-    activePiece = this.createPiece();
-    nextPiece = this.createPiece();
+    constructor () {
+        this.reset();
+    }
 
     get level() {
         return Math.floor(this.lines * 0.1);
@@ -41,8 +39,18 @@ export default class Game {
             level: this.level,
             lines: this.lines,
             nextPiece: this.nextPiece,
-            playfield
-        }
+            playfield,
+            isGameOver: this.topOut
+        };
+    }
+
+    reset() {
+        this.score = 0;
+        this.lines = 0;
+        this.topOut =  false;
+        this.playfield = this.createPlayfield();
+        this.activePiece = this.createPiece();
+        this.nextPiece = this.createPiece();
     }
 
     createPlayfield() {
@@ -176,6 +184,8 @@ export default class Game {
     }
 
     movePieceDown() {
+        if (this.topOut) return;
+
         this.activePiece.y +=1;
 
         if (this.hasCollision()) {
@@ -184,6 +194,10 @@ export default class Game {
             const clearedLines = this.clearLines();
             this.updateScore(clearedLines);
             this.updatePieces();
+        }
+
+        if (this.hasCollision()) {
+            this.topOut = true;
         }
     }
 
